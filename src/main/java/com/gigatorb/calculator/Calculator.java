a76605b64b52b6b047a0c3b125dbb6c7d1e72edf
Anupam <anupam.mishra@gigatorb.com>
2021-09-23 20:16:11 +0530

    Calling Add with a negative number will throw an exception "negatives not allowed" - and the negative that was passed.

--- src/main/java/com/gigatorb/calculator/Calculator.java
+++ src/main/java/com/gigatorb/calculator/Calculator.java
@@ -2,7 +2,7 @@ package com.gigatorb.calculator;
 
 public class Calculator {
 
-    public double addMe(String str){
+    public double addMe(String str) throws Exception{
         double output = 0;
         if(str == null || str.trim().length() == 0){
             return output;
@@ -19,14 +19,35 @@ public class Calculator {
             strArr = str.trim().replaceAll("\n",",")
                     .split(",");
         }
-
+        String negativeStr = "";
+        boolean isNegativeFound = false;
         for(String str1: strArr){
             if(str1.trim().length() > 0) {
-                output += Double.parseDouble(str1.trim());
+                double myNewNum = Double.parseDouble(str1.trim());
+                if(isNegativeNumber(myNewNum)){
+                    isNegativeFound = true;
+                    if(negativeStr.length() > 0) {
+                        negativeStr = negativeStr + " " + myNewNum;
+                    }else {
+                        negativeStr = myNewNum + "";
+                    }
+                }
+                output += myNewNum;
             }
+        }
+
+        if(isNegativeFound){
+            throw new Exception("negatives not allowed "+negativeStr);
         }
 
         return output;
 
+    }
+
+    private boolean isNegativeNumber(double number){
+        if(number < 0){
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/gigatorb/calculator/CalculatorTest.java
+++ src/test/java/com/gigatorb/calculator/CalculatorTest.java
@@ -8,7 +8,7 @@ public class CalculatorTest extends TestCase {
 
     // Test with empty string
     @Test
-    public void testWithEmptyString(){
+    public void testWithEmptyString() throws Exception{
         Calculator calculator = new Calculator();
         double num = calculator.addMe(" ");
         Assert.assertEquals(0, num, 0);
@@ -16,7 +16,7 @@ public class CalculatorTest extends TestCase {
 
     // Test with null String
     @Test
-    public void testWithNullString(){
+    public void testWithNullString() throws Exception{
         Calculator calculator = new Calculator();
         double num = calculator.addMe(null);
         Assert.assertEquals(0, num, 0);
@@ -24,7 +24,7 @@ public class CalculatorTest extends TestCase {
 
     //Test with multiple numbers
     @Test
-    public void testWithUnkownAmountOfNumbers(){
+    public void testWithUnkownAmountOfNumbers() throws Exception{
         Calculator calculator = new Calculator();
         double num = calculator.addMe("3,4,5,6");
         Assert.assertEquals(18, num, 0);
@@ -32,7 +32,7 @@ public class CalculatorTest extends TestCase {
 
     //Handling new lines between numbers
     @Test
-    public void testHandlingNewLines(){
+    public void testHandlingNewLines() throws Exception{
         Calculator calculator = new Calculator();
         double num = calculator.addMe("3\n4,5");
         Assert.assertEquals(12, num, 0);
@@ -40,11 +40,18 @@ public class CalculatorTest extends TestCase {
 
     //Support different delimiters
     @Test
-    public void testSupportDifferentDelimiters(){
+    public void testSupportDifferentDelimiters() throws Exception{
         Calculator calculator = new Calculator();
         double num = calculator.addMe("//;\n1;2");
         Assert.assertEquals(3, num, 0);
     }
 
+    //Support Negative Numbers
+    @Test
+    public void testWithNegativeNumber() throws Exception{
+        Calculator calculator = new Calculator();
+        double num = calculator.addMe("1,2,-5,5,-8");
+        Assert.assertEquals(3, num, 0);
+    }
 
 }
